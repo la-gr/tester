660bffed8d5febc161f8f0220e57da7c8dec872a
lara grahek <lara@grahek.net>
2025-03-18 13:38:51 -0400

    enrythinf

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { Server } from "socket.io";
-
-//const PORT = process.env.PORT || 3000;
-
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
-
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-app.use(express.static(join(__dirname, 'public')));
-
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-});
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
-
-server.listen(3000, () => {
-    console.log(`Server running at http://localhost:3000`);
-});
-
+window.onload = function () {
+    let but = document.getElementById("but");
+    if (but) {
+        but.onclick = function () {
+            window.location.href = "serv.html";
+        };
+    }
+};
